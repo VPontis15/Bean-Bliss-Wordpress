688b95184187aa5794a0522878266daa42a5f38b
VPontis <vpontis15@gmail.com>
2024-03-30 17:09:17 +0200

    add theme support for title

--- functions.php
+++ functions.php
@@ -3,6 +3,13 @@
 function bean_bliss_files(){
 wp_enqueue_style('bliss_styles', get_theme_file_uri() . '/assets/css/generic.css', array(), 'Version 1','all');
 wp_enqueue_style('bliss_styles-main', get_theme_file_uri() . '/assets/css/homepage.css', array(), 'Version 1','all');
+wp_enqueue_script('bliss_carousel', get_theme_file_uri( ) . '/assets/js/carousel.js', array(), 'Version 1', 'all');
 }
+
+function bean_bliss_features(){
+    add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'bean_bliss_features');
 
 add_action('wp_enqueue_scripts', 'bean_bliss_files');
