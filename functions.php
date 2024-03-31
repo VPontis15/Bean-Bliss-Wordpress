f39227a353681c87e3f46038edc737d45ba2f562
VPontis <vpontis15@gmail.com>
2024-03-31 14:05:47 +0300

    add custom type Events

--- functions.php
+++ functions.php
@@ -9,7 +9,32 @@ wp_enqueue_script('bliss_main-js', get_theme_file_uri( ) . '/assets/js/main.js',
 
 function bean_bliss_features(){
     add_theme_support('title-tag');
+    add_theme_support('post-thumbnail');
 }
+
+function events_post_type() {
+register_post_type( 'Events', array(
+    'supports'=> array('title', 'editor', 'thumbnail', 'excerpt'),
+    'labels' => array(
+        'name' => 'Events',
+        'singular_name' => 'Event',
+        'menu_name'=> 'Events',
+        'add_new' => "Add new Event",
+        'add_new_item' => "Add new Event",
+        'edit_item'=> "Edit Event",
+        "view_item"=> "View Event",
+        "search_items"=> "Search Events",
+        "not_found"=> "No events found"
+
+       
+    ), 'public'=> true,
+    'menu_icon'=> 'dashicons-calendar-alt',
+    'has_archive'=> true,
+    'show_in_rest' => true));
+
+}
+
+add_action('init', 'events_post_type');
 
 add_action('after_setup_theme', 'bean_bliss_features');
 
